de0344b7af7d69a17758a9b57742458f58d3d0b4
wg4466 <wg4466@GOL2000-14>
2023-02-04 22:56:29 -0500

    Add project files.

---
+++ Assets/Branch.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch: MonoBehaviour
+{
+    public float t;
+    public float startThickness;
+    public float maxThickness;
+   
+    public float growthRate;
+    public float branchLength;
+    public float branchStartLength;
+    public float branchThreshLength;
+    public float branchMaxLength;
+    public Vector2 branchVector;
+    public Vector2 normalizedLocation;
+    public float locationT;
+    public float startLocationT;
+    public float maxLocationT;
+    bool isGrowing = false;
+    public float desiredT = 0;
+    public GameObject startCircle;
+    public GameObject endCircle;
+    public GameObject box;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    public void initialize(float startThick,float maxThick,float branchStartLength,float branchThreshLength,float branchMaxLength,float locationT,float maxLocationT,Vector2 normalizedLocation,Vector2 branchVector)
+    {
+        startThickness = startThick;
+        maxThickness = maxThick;
+        branchLength = branchStartLength;
+        this.branchStartLength = branchStartLength;
+        this.branchThreshLength = branchThreshLength;
+        this.branchMaxLength = branchMaxLength;
+        this.locationT = locationT;
+        this.maxLocationT = maxLocationT;
+        this.startLocationT = locationT;
+        this.normalizedLocation = normalizedLocation;
+        this.branchVector = branchVector;
+        t = 0;
+
+        startCircle = transform.Find("startCircle").gameObject;
+        endCircle = transform.Find("endCircle").gameObject;
+        box = transform.Find("box").gameObject;
+        startCircle.transform.localPosition = new Vector2(0, 0);
+        startCircle.transform.localScale = new Vector2(startThick, startThick);
+        endCircle.transform.localScale = new Vector2(startThick, startThick);
+        endCircle.transform.localPosition = branchVector * branchStartLength;
+        box.transform.localPosition = branchVector * branchStartLength * 0.5f;
+        box.transform.localScale 
+        box.transform.rotation = Quaternion.FromToRotation(Vector3.up, new Vector3(branchVector.x, branchVector.y, 0.0f));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrowing)
+        {
+           t+= Time.deltaTime;
+            float thickness = Mathf.Min(Mathf.Lerp(startThickness, maxThickness, t), maxThickness);
+            float length = Mathf.Min(Mathf.Lerp(branchStartLength, branchMaxLength, t), branchMaxLength);
+            transform.localPosition = normalizedLocation * Mathf.Min(Mathf.Lerp(startLocationT, maxLocationT, t)); ;
+            startCircle.transform.localScale = new Vector2(thickness, thickness);
+            endCircle.transform.localScale = new Vector2(thickness, thickness);
+            endCircle.transform.localPosition = branchVector * length;
+            box.transform.localPosition = branchVector * length * 0.5f;
+            box.transform.rotation = Quaternion.FromToRotation(Vector3.up, new Vector3(branchVector.x, branchVector.y, 0.0f));
+            if (t > desiredT)
+            {
+                isGrowing = false;
+            }
+
+        }
+        
+    }
+    public void Grow(float inc)
+    {
+        desiredT += inc;
+        isGrowing = true;
+    }
+    
+        
+  
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Node<T>
+{
+    private List<Node<T>> branches;
+    private Node<T> parent;
+    private int id=0;
+    public delegate Dictionary<string, object> functionEval(Dictionary<string, object> args, Node<T> node);
+    private static int idOffset = 0;
+    private T content;
+    public Node(T content){
+        this.content = content;
+        branches = new List<Node<T>>();
+        id = idOffset;
+        idOffset++;
+    }
+    public T Get(){
+        return content;
+    }
+    public Node<T> getParent()
+    {
+        return parent;
+    }
+    public List<Node<T>> GetChildren(){
+        return branches;
+    }
+    public void Add(T content)
+    {
+        branches.Add(new Node<T>(content));
+        branches[branches.Count-1].parent = this;
+    }
+    public Dictionary<string, object> rRun(functionEval runFunc,Dictionary<string,object> args){
+        Dictionary<string, object> returnVal = runFunc(args,this);
+        for(int i =0;i<branches.Count;i++){
+            Dictionary<string, object> rValbranches = branches[i].rRun(runFunc, args);
+            foreach (var item in rValbranches)
+            {
+                returnVal.Add(id + "_" + item.Key, item.Value);
+            }
+        }
+        return returnVal;
+    }
+    public bool IsEndNode()
+    {
+        if(branches.Count == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public List<Node<T>> getEndNodes()
+    {
+        List<Node<T>> nodes = new List<Node<T>>();
+        if (IsEndNode())
+        {
+            nodes.Add(this);
+            return nodes;
+        }
+        for(int i = 0; i < branches.Count; i++)
+        {
+            List<Node<T>> branchNodes = branches[i].getEndNodes();
+            for(int k = 0; k < branchNodes.Count; k++)
+            {
+                nodes.Add(branchNodes[k]);
+            }
+        }
+        return nodes;
+    }
+    public Node<T> getNode(int dId)
+    {
+        if (dId == id)
+        {
+            return this;
+        }
+        for(int i = 0;i<branches.Count;i++)
+        {
+            Node<T> branch = branches[i].getNode(dId);
+            if(branch != null)
+            {
+                return branch;
+            }  
+        }
+        return null;
+    }
+    public int getParentCount(int current = 0)
+    {
+        if (parent != null)
+        {
+            return parent.getParentCount(current++);
+        }
+        return current;
+    }
+}
